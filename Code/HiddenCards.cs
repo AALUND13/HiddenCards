c66f61f64d5741f3dd6cbcdc7ae61ca467ecc5af
Autuwumn <112526071+Autuwumn@users.noreply.github.com>
2024-01-18 11:44:43 -0700

    Add files via upload

---
+++ Code/HiddenCards.cs
@@ -0,0 +1,95 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Jotunn.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnboundLib.Cards;
+using UnboundLib.GameModes;
+using UnityEngine;
+using UnboundLib;
+using BepInEx.Configuration;
+using UnboundLib.Utils.UI;
+using Photon.Pun;
+using UnboundLib.Networking;
+
+namespace HiddenCards
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, Version)]
+    [BepInProcess("Rounds.exe")]
+    public class HC : BaseUnityPlugin
+    {
+        private const string ModId = "com.ancientkoala.rounds.hiddencards";
+        private const string ModName = "Hidden Cards";
+        public const string Version = "0.0.1";
+        public const string ModInitials = "HC";
+
+        public static HC instance;
+
+        internal static AssetBundle ArtAssets;
+
+        private bool gameGoing = false;
+
+        public static ConfigEntry<bool> enabled;
+        public static bool tempEnable = true;
+
+        private void EnableCardsAction(bool val)
+        {
+            enabled.Value = val;
+        }
+        private void NewGUI(GameObject menu)
+        {
+            MenuHandler.CreateToggle(enabled.Value, "Hidden Mode", menu, EnableCardsAction);
+        }
+
+        void Start()
+        {
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+            instance = this;
+
+            Unbound.RegisterMenu("Hidden Cards", () => { }, this.NewGUI, null);
+            enabled = base.Config.Bind<bool>("HC", "enabled hidden", true);
+
+            GameModeManager.AddHook(GameModeHooks.HookGameStart, GameStart);
+            GameModeManager.AddHook(GameModeHooks.HookGameEnd, GameEnd);
+        }
+        void Update()
+        {
+            if (enabled.Value)
+            {
+                if (gameGoing)
+                {
+                    foreach (var p in PlayerManager.instance.players)
+                    {
+                        if (!p.data.view.IsMine) ModdingUtils.Utils.CardBarUtils.instance.ClearCardBar(p);
+                    }
+                }
+            }
+            
+        }
+        IEnumerator GameStart(IGameModeHandler gm)
+        {
+            gameGoing = true;
+            if (PhotonNetwork.IsMasterClient)
+            {
+                NetworkingManager.RPC_Others(typeof(HC), nameof(RPC_SyncSettings), enabled.Value);
+                tempEnable = enabled.Value;
+            }
+            yield return null;
+        }
+        IEnumerator GameEnd(IGameModeHandler gm)
+        {
+            gameGoing = false;
+            yield return null;
+        }
+        [UnboundRPC]
+        private static void RPC_SyncSettings(bool isEnabled)
+        {
+            tempEnable = isEnabled;
+        }
+    }
+
+}
---
+++ Code/Patches/UniqueCardChoicePatch.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using UnboundLib;
+using UnboundLib.Networking;
+using Photon.Pun;
+using UnityEngine.Assertions;
+using System.Linq;
+using UnboundLib.Utils;
+using HiddenCards;
+
+[HarmonyPatch(typeof(CardChoice))]
+public class UniqueCardChoicePatch
+{
+    [HarmonyPatch("SpawnUniqueCard")]
+    [HarmonyPostfix]
+    public static void SpawnUniqueCard_Patch(ref GameObject __result, int ___pickrID, PickerType ___pickerType, Vector3 pos, Quaternion rot)
+    {
+        // get currently picking player
+        Player player;
+        if (___pickerType == PickerType.Team)
+        {
+            player = PlayerManager.instance.GetPlayersInTeam(___pickrID)[0];
+        }
+        else
+        {
+            player = PlayerManager.instance.players[___pickrID];
+        }
+        var hide = true;
+        foreach(var p in PlayerManager.instance.players.Where((p) => p.data.view.IsMine)) if (p.teamID == player.teamID) hide = false;
+        if(hide && HC.tempEnable)
+        {
+            __result.transform.GetChild(0).GetChild(0).GetChild(0).gameObject.SetActive(false);
+            __result.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<CanvasGroup>().enabled = false;
+        }
+    }
+}
